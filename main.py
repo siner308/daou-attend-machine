b37501b8dda36f272b313fbf7375370cf37341b4
Siner <siner308@gmail.com>
2019-11-19 11:40:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,118 @@
-from selenium import webdriver
 import datetime
 import time
 import requests
+from selenium import webdriver
 
 
 TIMEOUT = 5
-URL = 'groupware url'
 USERNAME = 'username'
 PASSWORD = 'password'
 DRIVER_PATH = 'path of chromedriver'
+GW_URL = 'groupware url'
+ATTEND_URL = GW_URL + '/app/home'
+LOGIN_URL = GW_URL + '/login'
 
 
-def check_internet():
-    try:
-        _ = requests.get(URL, timeout=TIMEOUT)
+class AttendMachine:
+    def __init__(self):
+        print('Initializing attend machine...')
+        self.is_connected = False
+        self.driver = None
+
+    def check_internet(self):
+        print('Checking internet...')
+        try:
+            _ = requests.get(LOGIN_URL, timeout=TIMEOUT)
+            self.is_connected = True
+            print('Connected.')
+            return True
+        except requests.ConnectionError:
+            print("Internet connection is bad...")
+        return False
+
+    def get_login_page(self):
+        while not self.check_internet():
+            print('Waiting connection')
+            time.sleep(1)
+
+        self.driver.get(LOGIN_URL)
+
+        cnt = 0
+        while self.driver.title != '로그인' \
+                and self.driver.title != 'Sign In':
+            print('Connecting to login page...')
+            time.sleep(1)
+            cnt += 1
+
+            if cnt > 10:
+                print('Attempts Exceed. Please check Internet connection.')
+                return False
+
+        print('Connected.')
         return True
-    except requests.ConnectionError:
-        print("Internet connection is bad...")
-    return False
 
+    def get_attend_page(self):
+        print('Connecting to attend page...')
+        self.driver.get(ATTEND_URL)
 
-def get_chrome():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("headless")
-    chrome_options.add_argument("window-size=1920x1080")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("disable-gpu")
-    driver = webdriver.Chrome(DRIVER_PATH, chrome_options=chrome_options)
-    return driver
+        cnt = 0
+        while str(self.driver.title).find('DaouOffice') == -1:
+            time.sleep(1)
+            cnt += 1
 
+            if cnt > 10:
+                print('Attempts Exceed. Please check Internet connection.')
+                raise ConnectionError
+        return True
 
-def login(driver):
-    driver.get(URL)
-    driver.find_element_by_name('username').send_keys(USERNAME)
-    driver.find_element_by_name('password').send_keys(PASSWORD)
-    driver.find_element_by_xpath('//*[@id="loginForm"]/section/fieldset/a').click()
-    time.sleep(5)
-    return driver
+    def init_chrome(self):
+        if self.driver:
+            return True
 
+        print('Initializing web driver...')
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        options.add_argument("window-size=1920x1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("disable-gpu")
+        self.driver = webdriver.Chrome(DRIVER_PATH, options=options)
+        return True
 
-def attend_in():
-    driver = get_chrome()
-    driver = login(driver)
-    driver.find_element_by_xpath('//*[@id="attndGadgetClockIn"]').click()
-    print('attend in at %s' % str(datetime.datetime.now()))
+    def login(self):
+        self.get_login_page()
 
+        print('Sending username...')
+        self.driver.find_element_by_name('username').send_keys(USERNAME)
 
-def attend_out():
-    driver = get_chrome()
-    driver = login(driver)
-    driver.find_element_by_xpath('//*[@id="attndGadgetClockOut"]').click()
-    print('attend in out %s' % str(datetime.datetime.now()))
+        print('Sending password...')
+        self.driver.find_element_by_name('password').send_keys(PASSWORD)
+
+        print('Signing in...')
+        self.driver.find_element_by_xpath('//*[@id="loginForm"]/section/fieldset/a').click()
+        time.sleep(5)
+
+    def attend_in(self):
+        print('Attending in...')
+        self.get_attend_page()
+        self.driver.find_element_by_xpath('//*[@id="attndGadgetClockIn"]').click()
+        print('Attended in at %s' % str(datetime.datetime.now()))
+
+    def attend_out(self):
+        print('Attending out...')
+        self.get_attend_page()
+        self.driver.find_element_by_xpath('//*[@id="attndGadgetClockOut"]').click()
+        print('Attended out at %s' % str(datetime.datetime.now()))
 
 
 def main():
-    is_connected = False
+    machine = AttendMachine()
+    machine.init_chrome()
+    machine.login()
 
-    while not is_connected:
-        if check_internet():
-            is_connected = True
-            print('Internet connected')
-        else:
-            time.sleep(5)
-            print('Waiting internet connection')
-
-    if datetime.datetime.today().hour >= 18:
-        attend_out()
+    if not datetime.datetime.today().hour >= 18:
+        machine.attend_in()
     else:
-        attend_in()
+        machine.attend_out()
 
 
 if __name__ == "__main__":
